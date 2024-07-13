6be0295b3e1bae2e920b96a2b629c9f7d492b874
Marcos Costa <marcoscosta@air-de-marcos.lan>
2024-07-13 10:55:41 +0100

    Complete app

---
+++ practicing03/practicing03/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using Internal;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n, i = 0;
+
+            Console.WriteLine("Insira abaixo o numero: ");
+            n = int.Parse(Console.ReadLine());
+
+            int[] vet = new int[n];
+
+            string[] vetor = Console.ReadLine().Split(' ');
+
+            for (i = 0; i < n; i++)
+            {
+                vet[i] = int.Parse(vetor[i]);
+            }
+
+
+            int counter = 0;
+            for (i = 0; i < n; i++)
+            {
+                if (vet[i] % 2 == 0)
+                {
+                    counter++;
+                    Console.WriteLine(vet[i]);
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
